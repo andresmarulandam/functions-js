bee930b169d8780d0b3b602b598c97045f2f8366
Andres <marulandaandres19@gmail.com>
2023-03-14 19:10:45 -0500

    primeros ejercicios

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+// Define una función llamada greetings, que reciba cómo parámetros el nombre y el apellido de una persona y retorne lo siguiente:
+
+// Mi nombre es Nayib y mi apellido es Abdalá
+// Mi nombre es Germán y mi apellido es Escobar
+// Mi nombre es Simón y mi apellido es Hoyos
+
+function greetings (nombre,apellido){
+    return 'Mi nombre es ' + nombre +' y mi apellido es '+ apellido;
+
+}
+console.log(greetings("Nayib", "Abdalá"));
+console.log(greetings("Germán", "Escobar"));
+console.log(greetings("Simón", "Hoyos"));
---
+++ ejercicio2.js
@@ -0,0 +1,13 @@
+// Escribir una función llamada suma que reciba dos números y retorne su suma.
+
+function suma (numero1,numero2){
+    return ( numero1+numero2);
+
+
+}
+
+
+console.log(suma(10, 20));  // 30
+console.log(suma(10, 10));  // 20
+console.log(suma(40, 60));  // 100
+console.log(suma(50, 200));  // 250
